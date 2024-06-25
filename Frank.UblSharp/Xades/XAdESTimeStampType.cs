cf56926d6d85d7050965c8c4618d56069b648768
Frank R. Haugen <frank.haugen@gmail.com>
2024-06-25 17:52:36 +0200

    FFFF

--- Frank.UblSharp/Xades/XAdESTimeStampType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Frank.UblSharp.Xades;
-
-[System.Diagnostics.DebuggerStepThroughAttribute()]
-[System.Xml.Serialization.XmlTypeAttribute("XAdESTimeStamp", Namespace = "http://uri.etsi.org/01903/v1.3.2#")]
-[System.Xml.Serialization.XmlRootAttribute("XAdESTimeStamp", Namespace = "http://uri.etsi.org/01903/v1.3.2#", IsNullable=false)]
-public partial class XAdESTimeStampType : GenericTimeStampType
-{
-}
