dc5fece6a8906b34b2e2e4e0e6630861cd9e265b
Frank R. Haugen <frank.haugen@gmail.com>
2024-06-25 18:03:36 +0200

    uhouhl

--- Frank.UblSharp.GeneratorV2/Program.cs
+++ Frank.UblSharp.GeneratorV2/Program.cs
@@ -14,9 +14,9 @@ public class Program
     {
         var outputDirectory = new DirectoryInfo(@"D:\frankrepos\Frank.UblSharp\Frank.UblSharp");
         
-        GenerateFiles(outputDirectory);
+        // GenerateFiles(outputDirectory);
 
-        // await PropertyAutoPropertyConverter.ConvertAsync(outputDirectory);
+        await PropertyAutoPropertyConverter.ConvertAsync(outputDirectory);
     }
 
     private static void GenerateFiles(DirectoryInfo outputDirectory)
@@ -57,8 +57,63 @@ public static class PropertyAutoPropertyConverter
         var newRoot = new PropertyRewriter().Visit(root);
         return newRoot.ToString();
     }
+    
+    internal class PropertyRewriter : CSharpSyntaxRewriter
+{
+    public override SyntaxNode VisitPropertyDeclaration(PropertyDeclarationSyntax node)
+    {
+        if (node.AccessorList != null && node.AccessorList.Accessors.Count == 2)
+        {
+            var getAccessor = node.AccessorList.Accessors.FirstOrDefault(a => a.Kind() == SyntaxKind.GetAccessorDeclaration);
+            var setAccessor = node.AccessorList.Accessors.FirstOrDefault(a => a.Kind() == SyntaxKind.SetAccessorDeclaration);
 
-    private class PropertyRewriter : CSharpSyntaxRewriter
+            if (getAccessor != null && setAccessor != null &&
+                getAccessor.Body == null && setAccessor.Body == null)
+            {
+                var fieldName = GetBackingField(node);
+                if (fieldName != null)
+                {
+                    // Create a new property declaration with auto-property syntax and preserve the attributes
+                    var newProperty = node
+                        .WithAccessorList(SyntaxFactory.AccessorList(SyntaxFactory.List(new[]
+                        {
+                            SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
+                                .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)),
+                            SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
+                                .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken))
+                        })))
+                        .WithTrailingTrivia(SyntaxFactory.Whitespace(Environment.NewLine))
+                        .WithLeadingTrivia(node.GetLeadingTrivia())
+                        .WithTriviaFrom(node);
+
+                    return newProperty;
+                }
+            }
+        }
+
+        return base.VisitPropertyDeclaration(node);
+    }
+
+    private string? GetBackingField(PropertyDeclarationSyntax property)
+    {
+        var classDeclaration = property.Ancestors().OfType<ClassDeclarationSyntax>().FirstOrDefault();
+        if (classDeclaration == null) return null;
+
+        // Find the field with a similar name to the property
+        var fieldName = property.Identifier.Text;
+        var backingField = classDeclaration.Members
+            .OfType<FieldDeclarationSyntax>()
+            .FirstOrDefault(f =>
+            {
+                var fieldIdentifier = f.Declaration.Variables.First().Identifier.Text;
+                return string.Equals(fieldIdentifier.TrimStart('_'), fieldName, StringComparison.OrdinalIgnoreCase);
+            });
+
+        return backingField?.Declaration.Variables.First().Identifier.Text;
+    }
+}
+
+    private class PropertyRewriterV1 : CSharpSyntaxRewriter
     {
         public override SyntaxNode VisitPropertyDeclaration(PropertyDeclarationSyntax node)
         {
