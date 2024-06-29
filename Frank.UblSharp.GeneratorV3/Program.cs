0040b11bc3a29c0b66bbd6d6638cb8072cc8a1d1
Frank R. Haugen <frank.haugen@gmail.com>
2024-06-29 12:43:10 +0200

    Update XsdFilePath in UblSharp Generator
    The path for the XsdFile in the UblSharp Generator has been updated. The new location is now under the Resources folder, specifically in "D:\\frankrepos\\Frank.UblSharp\\Frank.UblSharp.Resources\\xsd\\maindoc\\UBL-Invoice-2.1.xsd". This change will ensure that the program is referencing the correct file location.

--- Frank.UblSharp.GeneratorV3/Program.cs
+++ Frank.UblSharp.GeneratorV3/Program.cs
@@ -4,7 +4,8 @@ var xsdExeOptions = new XsdExeOptions
 {
     OutputDirectory = new DirectoryInfo("D:\\frankrepos\\Frank.UblSharp\\Frank.UblSharp"),
     Namespace = "Frank.UblSharp",
-    XsdFilePath = new FileInfo("D:\\frankrepos\\Frank.UblSharp\\UBL-2.1\\exp\\UBL-Invoice-2.1.xsd")
+    XsdFilePath = new FileInfo("D:\\frankrepos\\Frank.UblSharp\\Frank.UblSharp.Resources\\xsd\\maindoc\\UBL-Invoice-2.1.xsd")
+    // XsdFilePath = new FileInfo("D:\\frankrepos\\Frank.UblSharp\\UBL-2.1\\exp\\UBL-Invoice-2.1.xsd")
     // XsdFilePath = new FileInfo("D:\\frankrepos\\Frank.UblSharp\\UBL-2.1\\xsdrt\\maindoc\\UBL-Invoice-2.1.xsd")
 };
 
